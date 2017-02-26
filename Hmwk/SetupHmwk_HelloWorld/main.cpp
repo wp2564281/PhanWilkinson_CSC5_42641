ff32910ba9e6c18afe3d05dd6b8313b880566b15
wp2564281 <wilkinsonhp@yahoo.com>
2017-02-25 17:15:30 -0800

    Signed-off-by: wp2564281 <wilkinsonhp@yahoo.com>

---
+++ Hmwk/SetupHmwk_HelloWorld/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Wilkinson Phan
+ * Created on February 25, 2017, 5:06 PM
+ * Purpose: To create a simple Hello World program
+ * 
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    cout << "Hello World!";
+    //Exit stage right!
+    return 0;
+}
+
