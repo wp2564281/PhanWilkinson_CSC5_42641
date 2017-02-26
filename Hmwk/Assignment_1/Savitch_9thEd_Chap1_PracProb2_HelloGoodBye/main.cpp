ee0344e34a2a11a2ae8c2cbf2a320adfe2222d99
wp2564281 <wilkinsonhp@yahoo.com>
2017-02-26 15:08:52 -0800

    Signed-off-by: wp2564281 <wilkinsonhp@yahoo.com>

---
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_PracProb2_HelloGoodBye/main.cpp
@@ -0,0 +1,51 @@
+/* 
+  File:   main.cpp
+  Author: Wilkinson Phan
+  Created on February 26, 2017, 2:52 PM
+  Purpose:  Add "Hello" and "Good-bye" to PracProb1.
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Such as PI, Vc, -> Math/Science values
+//as well as conversions from system of units to 
+//another
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    int number_of_pods, peas_per_pod, total_peas;
+    
+    //Input values
+    cout << "Hello\n";
+    cout << "Press return after entering a number.\n";
+    cout << "Enter the number of pods:\n";
+    cin >> number_of_pods;
+    cout << "Enter the number of peas in a pod:\n";
+    cin >> peas_per_pod;
+    
+    //Process by mapping inputs to outputs
+    total_peas = number_of_pods*peas_per_pod;
+    
+    //Output values
+    cout << "If you have ";
+    cout << number_of_pods;
+    cout << " pea pods\n";
+    cout << "and ";
+    cout << peas_per_pod;
+    cout << " peas in each pod, then\n";
+    cout << "you have ";
+    cout << total_peas;
+    cout << " peas in all the pods.\n";
+    cout << "Good-bye\n";
+    
+    //Exit stage right!
+    return 0;
+}
