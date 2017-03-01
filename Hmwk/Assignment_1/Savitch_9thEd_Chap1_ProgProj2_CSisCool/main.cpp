1a50341eabc131652f1ca6b2585faaacdf0fd296
wp2564281 <wilkinsonhp@yahoo.com>
2017-02-28 18:48:21 -0800

    Signed-off-by: wp2564281 <wilkinsonhp@yahoo.com>

---
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_ProgProj2_CSisCool/main.cpp
@@ -0,0 +1,47 @@
+/* 
+  File:   main.cpp
+  Author: Wilkinson Phan
+  Created on February 28, 2017, 6:26 PM
+  Purpose:  To print out "CS!" in large block letters inside a
+ *          a border of *'s followed by the message "Computer 
+ *          Science is Cool Stuff!!!".
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Such as PI, Vc, -> Math/Science values
+//as well as conversions from system of units to 
+//another
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input values
+    
+    //Process by mapping inputs to outputs
+    
+    //Output values
+    cout <<"************************************************\n"<<endl;
+    cout <<"          C C C           S S S S        !!\n";
+    cout <<"        C       C       S         S      !!\n";
+    cout <<"       C               S                 !!\n";
+    cout <<"      C                 S                !!\n";
+    cout <<"      C                   S S S S        !!\n";
+    cout <<"      C                           S      !!\n";
+    cout <<"       C                           S     !!\n";
+    cout <<"        C       C       S         S\n";
+    cout <<"          C C C           S S S S        00\n"<<endl;
+    cout <<"************************************************\n";
+    cout <<"       Computer Science is Cool Stuff!!!\n"<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
